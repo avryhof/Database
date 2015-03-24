a2510cc88e3b87363b3321b06d35906daf1110cb
Amos Vryhof <amos@vryhofresearch.com>
2015-03-24 15:18:43 -0400

    Script Version Control
    Changes script versions to sit in an array at the top of the file.  That
    way versions can be configured in one place.

--- database-browser.php
+++ database-browser.php
@@ -21,7 +21,15 @@
       'version'   => '3.3.2',
       'theme'     => 'default'
     );
-  } 
+  }
+  
+  $libs = array(
+    'jquery'        => '1',
+    'fontawesome'   => '4.3.0',
+    'codemirror'    => '5.1.0',
+    'respond'       => '1.4.2',
+    'html5shiv'     => '3.7.2'
+  ); 
 	
 	if ($_POST['action'] == "execute_sql") {
 		
@@ -82,9 +90,9 @@
     <link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/bootswatch/<?= $bootstrap['version']; ?>/<?= $bootstrap['theme']; ?>/bootstrap.min.css">
     <? } ?>
     
-    <link href="//maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css" rel="stylesheet">
-    <link href="//cdnjs.cloudflare.com/ajax/libs/codemirror/4.12.0/codemirror.min.css" rel="stylesheet">
-    <link href="//cdnjs.cloudflare.com/ajax/libs/codemirror/5.1.0/addon/hint/show-hint.min.css" rel="stylesheet">
+    <link href="//maxcdn.bootstrapcdn.com/font-awesome/<?= $libs['fontawesome']; ?>/css/font-awesome.min.css" rel="stylesheet">
+    <link href="//cdnjs.cloudflare.com/ajax/libs/codemirror/<?= $libs['codemirror']; ?>/codemirror.min.css" rel="stylesheet">
+    <link href="//cdnjs.cloudflare.com/ajax/libs/codemirror/<?= $libs['codemirror']; ?>/addon/hint/show-hint.min.css" rel="stylesheet">
 
     <!-- Custom styles for this template -->
     <style>
@@ -99,8 +107,8 @@
 
     <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
     <!--[if lt IE 9]>
-      <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
-      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+      <script src="https://oss.maxcdn.com/html5shiv/<?= $libs['html5shiv']; ?>/html5shiv.min.js"></script>
+      <script src="https://oss.maxcdn.com/respond/<?= $libs['respond']; ?>/respond.min.js"></script>
     <![endif]-->
   </head>
 
@@ -410,12 +418,14 @@ if ($action == "add" || $action == "edit") {
     <!-- Bootstrap core JavaScript
     ================================================== -->
     <!-- Placed at the end of the document so the pages load faster -->
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1/jquery.min.js"></script>
-    <script src="//maxcdn.bootstrapcdn.com/bootstrap/3.3.2/js/bootstrap.min.js"></script>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/<?= $libs['jquery']; ?>/jquery.min.js"></script>
+    <script src="//maxcdn.bootstrapcdn.com/bootstrap/<?= $bootstrap['version']; ?>/js/bootstrap.min.js"></script>
     <? if ($show == "sql") { ?>
-    <script src="//cdnjs.cloudflare.com/ajax/libs/codemirror/4.12.0/codemirror.min.js"></script>
-    <script src="//cdnjs.cloudflare.com/ajax/libs/codemirror/5.0.0/mode/sql/sql.min.js"></script>
-    <script src="//cdnjs.cloudflare.com/ajax/libs/codemirror/5.0.0/addon/hint/sql-hint.min.js"></script>
+    <script src="//cdnjs.cloudflare.com/ajax/libs/codemirror/<?= $libs['codemirror']; ?>/codemirror.min.js"></script>
+    <script src="//cdnjs.cloudflare.com/ajax/libs/codemirror/<?= $libs['codemirror']; ?>/mode/sql/sql.min.js"></script>
+    <script src="//cdnjs.cloudflare.com/ajax/libs/codemirror/<?= $libs['codemirror']; ?>/addon/selection/selection-pointer.min.js"></script>
+    <script src="//cdnjs.cloudflare.com/ajax/libs/codemirror/<?= $libs['codemirror']; ?>/addon/hint/show-hint.min.js"></script>
+    <script src="//cdnjs.cloudflare.com/ajax/libs/codemirror/<?= $libs['codemirror']; ?>/addon/hint/sql-hint.min.js"></script>
     <script>
 	var mime = 'text/x-mariadb';
 	window.editor = CodeMirror.fromTextArea(document.getElementById("sql"), {
